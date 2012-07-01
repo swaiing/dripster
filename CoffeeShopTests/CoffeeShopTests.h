8689ec4c5101743d70eeee3bd83a5a0e040fb48b
swaiing <stevewai@gmail.com>
2012-07-01 10:28:59 -0400

    Initial Commit

---
+++ CoffeeShopTests/CoffeeShopTests.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeShopTests.h
+//  CoffeeShopTests
+//
+//  Created by Stephen Wai on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoffeeShopTests : SenTestCase
+
+@end
---
+++ CoffeeShopTests/CoffeeShopTests.m
@@ -0,0 +1,32 @@
+//
+//  CoffeeShopTests.m
+//  CoffeeShopTests
+//
+//  Created by Stephen Wai on 7/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoffeeShopTests.h"
+
+@implementation CoffeeShopTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoffeeShopTests");
+}
+
+@end
